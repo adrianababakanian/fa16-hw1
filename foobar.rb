4b8cc6b4b5f290c3a9b73c14ea514f697d165e07
Adriana Babakanian <ababakanian@berkeley.edu>
2016-09-21 16:09:07 -0700

    hw1 submission

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,14 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+
+    copy = a.map { |i| i.to_i }
+    add = copy.map { |i| i + 2}
+    even = add.delete_if {|i| i.odd? }
+    greater = even.delete_if {|i| i > 10}
+    sum = 0
+    greater.each { |a| sum += a }
+    sum
+
   end
 end
-
-
